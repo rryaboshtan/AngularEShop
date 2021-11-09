bcf2bdb0dfb175cadad4126b14acfcc269eeb768
rryaboshtan <rrvworkroman@gmail.com>
2021-11-09 02:38:32 +0200

    add layout components

---
+++ src/app/site-layout/site-layout.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SiteLayoutModule { }
