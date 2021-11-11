4ef2a4bd4ff9d914711e6406d2f093190eb86210
rryaboshtan <rrvworkroman@gmail.com>
2021-11-12 00:57:10 +0200

    add card to the view product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListOrderComponent } from './orders/list-order/list-order.component';
 
-const routes: Routes = [{ path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }];
+const routes: Routes = [{ path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+                        {path: 'order', component:ListOrderComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
