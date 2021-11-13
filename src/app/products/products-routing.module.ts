0caef5f9d28110598da02af54eb6c351871c3e26
rryaboshtan <rrvworkroman@gmail.com>
2021-11-14 01:17:36 +0200

    add bootstrap carousel

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductsComponent } from './products.component';
+import { ViewAllProductComponent } from './view-all-product/view-all-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
   { path: 'add-product', component: AddProductComponent },
+  { path: 'view-product', component: ViewProductComponent },
+  { path: 'list-product', component: ViewAllProductComponent },
 ];
 
 @NgModule({
