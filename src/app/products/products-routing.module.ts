41ad6e027523973ffb0217515fdde81a000294e7
rryaboshtan <rrvworkroman@gmail.com>
2021-11-14 01:28:51 +0200

    update paths

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
 import { ProductsComponent } from './products.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
 import { ViewAllProductComponent } from './view-all-product/view-all-product.component';
+import { ViewProductByCategoryComponent } from './view-product-by-category/view-product-by-category.component';
+import { ViewProductByDateComponent } from './view-product-by-date/view-product-by-date.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 
 const routes: Routes = [
@@ -10,6 +14,11 @@ const routes: Routes = [
   { path: 'add-product', component: AddProductComponent },
   { path: 'view-product', component: ViewProductComponent },
   { path: 'list-product', component: ViewAllProductComponent },
+  { path: 'search', component: ViewProductByCategoryComponent },
+  { path: 'search', component: ViewProductByDateComponent },
+  { path: 'delete-product', component: DeleteProductComponent },
+  { path: 'view-product/:id', component: ViewProductComponent },
+  { path: 'update-product/:id', component: UpdateProductComponent },
 ];
 
 @NgModule({
